282ffb65b975fca59f5c27f65273e831458cc1db
Camilooox <160759677+Camilooox@users.noreply.github.com>
2024-03-13 11:00:39 -0500

    Add files via upload

---
+++ While/main2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	
+   int i=1;
+
+   while(i<=6)
+   
+   {
+      cout<<"\n"<<"Value of variable i is: "<<i<<endl; i++;
+      
+   }
+   
+   return 0;
+   
+}
